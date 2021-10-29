729e6f258b50b4153e9381ffd75b22a6bea27f14
ARTH PRAJAPATI <76396335+kingap7@users.noreply.github.com>
2021-10-29 16:53:05 +0530

    Update MakeprettyTable.py

--- MakeprettyTable.py
+++ MakeprettyTable.py
@@ -2,11 +2,11 @@ from prettytable import PrettyTable
 
 myTable = PrettyTable(["Er_No.", "Name", "Skill"])
 
-myTable.add_row(["190220131103", "Arth", "Coding"])
-myTable.add_row(["190220131026", "Bhavya", "Teaching"])
-myTable.add_row(["190220131031", "Hiten", "Bakchodi"])
-myTable.add_row(["190220131094", "Vandan", "Astha"])
-myTable.add_row(["190220131134", "Yash", "OnedayMatch"])
-myTable.add_row(["190220131148", "Satyendrasinh", "keepQuite"])
+myTable.add_row(["DATA1", "DATA2", "DATA3"])
+myTable.add_row(["DATA1", "DATA2", "DATA3"])
+myTable.add_row(["DATA1", "DATA2", "DATA3"])
+myTable.add_row(["DATA1", "DATA2", "DATA3"])
+myTable.add_row(["DATA1", "DATA2", "DATA3"])
+myTable.add_row(["DATA1", "DATA2", "DATA3"])
 
 print(myTable)
